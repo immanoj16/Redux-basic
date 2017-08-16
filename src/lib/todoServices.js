0a410e922277bfb58e3401b7adef58567cccba02
immanoj16 <mmaharana14@gmail.com>
2017-08-16 10:01:15 +0530

    Save New Records on The Server with Redux Thunk and Fetch

--- src/lib/todoServices.js
+++ src/lib/todoServices.js
@@ -2,3 +2,15 @@ export const getTodos = () => {
   return fetch('http://localhost:8081/todos')
     .then(response => response.json())
 }
+
+export const createTodo = (name) => {
+  return fetch('http://localhost:8081/todos', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: name, isComplete: false})
+  })
+    .then(response => response.json())
+}
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import { getTodos } from "../lib/todoServices"
+import {createTodo, getTodos} from "../lib/todoServices"
 
 const TODO_ADD = 'TODO_ADD'
 const TODOS_LOAD = 'TODOS_LOAD'
@@ -8,10 +8,19 @@ export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
 
 export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
 
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
+
 export const fetchTodos = () => {
   return (dispatch) => {
     getTodos()
       .then(todos => dispatch(loadTodos(todos)))
+  }
+}
+
+export const saveTodo = (name) => {
+  return (dispatch) => {
+    createTodo(name)
+      .then(response => dispatch(addTodo(response)))
   }
 }
 
@@ -23,7 +32,7 @@ const initState = {
 export default (state = initState, action) => {
   switch (action.type) {
     case TODO_ADD:
-      return {...state, todos: state.todos.concat(action.payload)}
+      return {...state, currentTodo: '', todos: state.todos.concat(action.payload)}
 
     case CURRENT_UPDATE:
       return {...state, currentTodo: action.payload}
