46b94e1502a77c562a2de476a92615bb1c2ccc91
immanoj16 <mmaharana14@gmail.com>
2017-08-16 08:02:56 +0530

    Add: Provider and connect to connect the store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
+import {updateCurrent} from "./reducers/todo"
 
 class App extends Component {
   render() {
@@ -15,7 +19,7 @@ class App extends Component {
         <div className="Todo-App">
           <TodoForm
             currentTodo={this.props.currentTodo}
-            changeCurrent={this.props.changeCurrent} />
+            changeCurrent={this.props.updateCurrent} />
           <TodoList todos={this.props.todos} />
         </div>
       </div>
@@ -23,4 +27,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => state
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({updateCurrent}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
-import { bindActionCreators } from 'redux'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
-import {updateCurrent} from "./reducers/todo"
 
-// const todoChangehandler = (val) => store.dispatch(updateCurrent(val))
-
-const actions = bindActionCreators({updateCurrent}, store.dispatch)
-
-const render = () => {
-  const state = store.getState()
-  ReactDOM.render(
-    <App todos={state.todos} currentTodo={state.currentTodo} changeCurrent={actions.updateCurrent}/>,
-    document.getElementById('root'));
-}
-
-render()
-
-store.subscribe(render)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 registerServiceWorker();
