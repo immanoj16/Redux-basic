c57cf98d9e58b502530f3e3b23d82cb90dcad0a1
immanoj16 <mmaharana14@gmail.com>
2017-08-16 07:47:13 +0530

    Add: bindActionCreator

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { bindActionCreators } from 'redux'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 import {updateCurrent} from "./reducers/todo"
 
-const todoChangehandler = (val) => store.dispatch(updateCurrent(val))
+// const todoChangehandler = (val) => store.dispatch(updateCurrent(val))
+
+const actions = bindActionCreators({updateCurrent}, store.dispatch)
 
 const render = () => {
   const state = store.getState()
   ReactDOM.render(
-    <App todos={state.todos} currentTodo={state.currentTodo} changeCurrent={todoChangehandler}/>,
+    <App todos={state.todos} currentTodo={state.currentTodo} changeCurrent={actions.updateCurrent}/>,
     document.getElementById('root'));
 }
 
